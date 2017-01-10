8ecb982aba44291152614c7caf7e4e77dcc05058
Kevin Jung <me@kjung.ca>
2017-01-09 23:44:43 -0500

    Updated readme and added userscript code

---
+++ Gazelle Hide Filled on Request.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+//
+// @name         PTH/Apollo Hide Filled on Request
+// @version      0.1
+// @description  Hide filled requests on the request page.
+// @author       kjung
+// @include      http*://*apollo.rip/*
+// @include      http*://*passtheheadphones.me/*
+// @grant        none
+// @namespace https://greasyfork.org/en/users/6863
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function getRequestUrl() {
+        var hostname = window.location.hostname;
+
+        var requestUrl = 'requests.php?submit=true&search=&tags=&tags_type=1&releases%5B%5D=1&releases%5B%5D=3&releases%5B%5D=5&releases%5B%5D=6&releases%5B%5D=7&releases%5B%5D=9&releases%5B%5D=11&releases%5B%5D=13&releases%5B%5D=14&releases%5B%5D=15&releases%5B%5D=16&releases%5B%5D=17&releases%5B%5D=18&releases%5B%5D=19&releases%5B%5D=21&formats%5B%5D=0&formats%5B%5D=1&formats%5B%5D=2&formats%5B%5D=3&formats%5B%5D=4&bitrates%5B%5D=0&bitrates%5B%5D=1&bitrates%5B%5D=2&bitrates%5B%5D=3&bitrates%5B%5D=4&bitrates%5B%5D=5&bitrates%5B%5D=6&bitrates%5B%5D=7&bitrates%5B%5D=8&bitrates%5B%5D=9&bitrates%5B%5D=10&media%5B%5D=0&media%5B%5D=1&media%5B%5D=2&media%5B%5D=3&media%5B%5D=4&media%5B%5D=5&media%5B%5D=6&media%5B%5D=7&media%5B%5D=8';
+
+        if (hostname === 'apollo.rip') {
+            requestUrl = 'requests.php?submit=true&search=&tags=&tags_type=1&releases%5B%5D=1&releases%5B%5D=3&releases%5B%5D=5&releases%5B%5D=6&releases%5B%5D=7&releases%5B%5D=9&releases%5B%5D=11&releases%5B%5D=13&releases%5B%5D=14&releases%5B%5D=15&releases%5B%5D=16&releases%5B%5D=21&formats%5B%5D=0&formats%5B%5D=1&formats%5B%5D=2&formats%5B%5D=3&formats%5B%5D=4&formats%5B%5D=5&bitrates%5B%5D=0&bitrates%5B%5D=1&bitrates%5B%5D=2&bitrates%5B%5D=3&bitrates%5B%5D=4&bitrates%5B%5D=5&bitrates%5B%5D=6&bitrates%5B%5D=7&bitrates%5B%5D=8&bitrates%5B%5D=9&bitrates%5B%5D=10&bitrates%5B%5D=11&media%5B%5D=0&media%5B%5D=1&media%5B%5D=2&media%5B%5D=3&media%5B%5D=4&media%5B%5D=5&media%5B%5D=6&media%5B%5D=7&media%5B%5D=8';
+        }
+
+        return requestUrl;
+    }
+
+    var request = document.getElementById('nav_requests');
+    var requestAnchor = request.querySelector("a");
+    requestAnchor.href = getRequestUrl();
+})();
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# gazelle-hide-filled-on-request
-Userscript to deselect include filled on the request page.
+# Gazelle Hide Filled on Request Userscript
+## Introduction
+**Gazelle Hide Filled on Request** is a userscript to uncheck the "include filled" checkbox on the request page.
+
+## Compatibility
+Compatiable with [Tampermonkey](http://tampermonkey.net/) and Google Chrome.
+
+## Install
+Install by visiting [Greasy Fork](https://greasyfork.org/en/scripts/26412-pth-apollo-hide-filled-on-request) or navigate to the `raw` version of the `Gazelle Hide Filled on Request.user.js` file in this repository to trigger the Tampermonkey install prompt.
